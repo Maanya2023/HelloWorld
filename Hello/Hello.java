e159b6ede1dc606510d5d1341474df87d02d2e6d
Fareena Syed <fareena.syed@wissen.com>
2023-12-14 14:38:50 +0530

    push

--- Hello/Hello.java
+++ Hello/Hello.java
@@ -1,8 +1,6 @@
 package Hello;
 
-import java.sql.SQLOutput;
-
-public class Java {
+public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
